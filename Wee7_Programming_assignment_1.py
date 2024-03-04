7153ed04c724a94b7c6189e5773bc411258870db
AmitavaDatta2004 <145762757+AmitavaDatta2004@users.noreply.github.com>
2024-03-04 22:51:19 +0530

    Delete Wee7_Programming_assignment_1.py

--- Wee7_Programming_assignment_1.py
+++
@@ -1,7 +0,0 @@
-Label_Number="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Label=input(str())
-Number=0
-for i in range(1,len(Label)+1):
-    Value=int((Label_Number.index(Label[-i])+1)*((26)**(i-1)))
-    Number=Number+Value
-print(Number,end="")
